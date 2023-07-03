37fa1a2a3dc305925b9de90350282ad421f24c33
Reza Pramita <rezapramita83@gmail.com>
2023-07-03 18:14:42 +0700

    api

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:testflutter/bloc/bloc/get_data_item_bloc.dart';
+import 'package:testflutter/home_page.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => GetDataItemBloc(),
+      child: MaterialApp(
+        title: 'Test Flutter',
+        theme: ThemeData(
+          bottomSheetTheme:
+              const BottomSheetThemeData(backgroundColor: Colors.white),
+
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
+          primarySwatch: Colors.blue,
+        ),
+        home: const HomePage(),
+        // home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      ),
+    );
+  }
+}
